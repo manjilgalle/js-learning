cf8821952ae1168db196eb732e2e1e691207de76
Manjil Galle <manjilgalle20@gmail.com>
2024-03-08 11:31:34 +0530

    Today First Assignment
    Today First Assignment

---
+++ 01_variableAssign.js
@@ -0,0 +1,29 @@
+console.log("========================STEP I=========================");
+var myFullName;
+console.log("Before Initialization", myFullName);
+myFullName = "Manjil Galle";
+console.log("My Full Name :", myFullName);
+myFullName = "Manjil Sharadrao Galle";
+console.log("Update Full Name :", myFullName);
+//console.log("-------------------------------------------------------------------------");
+console.log("========================STEP II=========================");
+var myLove = "I Love Only JS";
+console.log(myLove);
+//console.log("-------------------------------------------------------------------------");
+console.log("========================STEP III=========================");
+var firstName = "Manjil";
+console.log("First Name :", firstName);
+var lastName = "Galle";
+console.log("Last Name :", lastName);
+var age = "100";
+console.log("My Age :", age);
+var address = "At. Post Shendola Khurd Tq. Teosa Dist Amravati"
+console.log("My Address : ", address);
+var pinCode = "444902";
+console.log("My Pin Code :", pinCode);
+var gender = "Male";
+console.log("My Gender :", gender);
+var isMarried = "No";
+console.log("Married :", isMarried);
+
+
