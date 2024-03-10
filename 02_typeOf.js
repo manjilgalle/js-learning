49dece7b42ef93c9e0a5985d4ea1b32db7c0dc62
Manjil Galle <manjilgalle20@gmail.com>
2024-03-10 13:27:56 +0530

    Data Type Assignment
    Data Type Assignment

---
+++ 02_typeOf.js
@@ -0,0 +1,38 @@
+console.log("---------------------------STEP I -----------------------------------");
+var bankName;
+console.log("Bank Name Value before Initialize         :", bankName);
+var typeOf = typeof bankName;
+console.log("Data Type of Bank Name before Initialize  :", typeOf);
+bankName = 'SBI Bank';
+console.log("Bank Name Value After Initialize          :", bankName);
+var typeOf = typeof bankName;
+console.log("Data Type of Bank Name After Initialize   :", typeOf);
+console.log("---------------------------STEP II -----------------------------------");
+var mark = '90%';
+console.log("Mark                                      :",mark );
+var marktype = typeof mark;
+console.log("Data Type of Mark                         :", marktype);
+console.log("---------------------------STEP III -----------------------------------");
+var isWorking;
+console.log("Is Working Value Before Initialize        :", isWorking );
+var typeWork = typeof isWorking;
+console.log("Data Type Working Before Initialize       :", typeWork );
+isWorking = true;
+console.log("Is Working Value After Initialize         :", isWorking );
+var typeWork = typeof isWorking;
+console.log("Data Type Working After Initialize        :", typeWork );
+console.log("---------------------------STEP IV -----------------------------------");
+var totalCount = "Hundred and seven";
+console.log("Total Count Value                         :", totalCount);
+var typeDataCount = typeof totalCount;
+console.log("Data Type Total Count                     :", typeDataCount);
+
+var totalCount = 107;
+console.log("Updating Total Count Value                :", totalCount);
+var typeDataCount = typeof totalCount;
+console.log("Updating Data Type Total Count            :", typeDataCount);
+
+
+
+
+
